069975a48c30f7f455d24217c3abd9d6591a5033
Colin COËRCHON <99185713+ColinAlAskA@users.noreply.github.com>
2023-07-12 22:06:48 +0200

    Alaska - commit du 12/07
    Commit de fin de journée.
    Belle avancée sur l'ajout de bilan pour les profs, le travail n'est pas encore fini.

--- lib/screens/action_buttons_prof/bilan_screen.dart
+++ lib/screens/action_buttons_prof/bilan_screen.dart
@@ -1,11 +1,349 @@
 
 import 'package:flutter/material.dart';
-import '../../utilities/constantes.dart';
 
+import '../../utilities/constantes.dart';
 import '../app_bar.dart';
 
 
-// Ici, déplacez le contenu du body de BilanScreen dans BilanContent
+class EleveInfoBlock extends StatelessWidget {
+  // TODO: Ajouter les champs d'information nécessaires en tant que variables finales ici
+
+  const EleveInfoBlock({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.95,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Élève',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'NotoSans',
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: const Padding(
+              padding: EdgeInsets.all(8),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("Identifiant : "),
+                        Text("Nom : "),
+                        Text("Prénom : "),
+                        Text("Civilité : "),
+                        Text("Date de n. : "),
+                        Text("Classe : "),
+                        Text("Int / Ext : "),
+                        Text("Etat : "),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text("H9GZH3"),
+                        Text("GAJENDRAN"),
+                        Text("Ajanthan"),
+                        Text("M"),
+                        Text("03/03/2005"),
+                        Text("Term G"),
+                        Text("CTEF Admin"),
+                        Text(
+                          "Active",
+                          style: TextStyle(
+                              color: Colors.green
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Image(
+                      image: AssetImage('assets/smiley/1.png'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+class RatingCell extends StatefulWidget {
+  final int value;
+  final ValueChanged<int?> onChanged;
+  final int groupValue;
+
+  const RatingCell({
+    Key? key,
+    required this.value,
+    required this.onChanged,
+    required this.groupValue,
+  }) : super(key: key);
+
+  @override
+  _RatingCellState createState() => _RatingCellState();
+}
+
+class _RatingCellState extends State<RatingCell> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Radio<int>(
+        value: widget.value,
+        groupValue: widget.groupValue,
+        onChanged: widget.onChanged,
+      ),
+    );
+  }
+}
+
+class BaseDeNotationBlock extends StatefulWidget {
+  const BaseDeNotationBlock({Key? key}) : super(key: key);
+
+  @override
+  _BaseDeNotationBlockState createState() => _BaseDeNotationBlockState();
+}
+
+
+class _BaseDeNotationBlockState extends State<BaseDeNotationBlock> {
+
+  int _groupValue1 = 0;
+  int _groupValue2 = 0;
+  int _groupValue3 = 0;
+  int _groupValue4 = 0;
+
+  TableRow _createRatingRow(String title, int groupValue, ValueChanged<int?> onChanged) {
+    return TableRow(
+      children: [
+        Center(child: Text(title)),
+        for (int value = 1; value <= 5; value++)
+          RatingCell(value: value, groupValue: groupValue, onChanged: onChanged),
+      ],
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionallySizedBox(
+      widthFactor: 0.95,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Base de Notation',
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'NotoSans',
+                ),
+              ),
+            ),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10),
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: const Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Table(
+              columnWidths: const <int, TableColumnWidth>{
+                0: FlexColumnWidth(1),
+                1: FlexColumnWidth(0.3),
+                2: FlexColumnWidth(0.3),
+                3: FlexColumnWidth(0.3),
+                4: FlexColumnWidth(0.3),
+                5: FlexColumnWidth(0.3),
+              },
+              border: TableBorder.all(
+                color: Colors.black26,
+                width: 1,
+              ),
+              children: [
+                TableRow(
+                  children: [
+                    const Center(child: Text(' ')),
+                    for (int i = 1; i <= 5; i++)
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: Image.asset('assets/smiley/$i.png', height: 30),
+                        ),
+                      ),
+                  ],
+                ),
+
+                _createRatingRow('Note globale', _groupValue1, (value) { setState(() { _groupValue1 = value ?? _groupValue1; }); }),
+                _createRatingRow('Note comportement', _groupValue2, (value) { setState(() { _groupValue2 = value ?? _groupValue2; }); }),
+                _createRatingRow('Note assiduité', _groupValue3, (value) { setState(() { _groupValue3 = value ?? _groupValue3; }); }),
+                _createRatingRow('Devoirs / DM faits', _groupValue4, (value) { setState(() { _groupValue4 = value ?? _groupValue4; }); }),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+class BilanBlock extends StatelessWidget {
+  // TODO: Ajouter les champs d'information nécessaires en tant que variables finales ici
+
+  const BilanBlock({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO: Ajoutez ici le code pour construire votre bloc d'information
+    );
+  }
+}
+
+
+class SoumettreButton extends StatelessWidget {
+  // TODO: Ajouter les champs d'information nécessaires en tant que variables finales ici
+
+  const SoumettreButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO: Ajoutez ici le code pour construire votre bloc d'information
+    );
+  }
+}
+
+
+
+
+
+class NewBilan extends StatefulWidget {
+  const NewBilan({Key? key}) : super(key: key);
+
+  @override
+  _NewBilanState createState() => _NewBilanState();
+}
+
+class _NewBilanState extends State<NewBilan> {
+  // TODO: Ajouter ici vos contrôleurs de champ de formulaire, vos variables d'état, etc.
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(color: orangePerso, context: context),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 20),
+            EleveInfoBlock(),
+            SizedBox(height: 20),
+            BaseDeNotationBlock(),
+            SizedBox(height: 20),
+            BilanBlock(),
+            SizedBox(height: 20),
+            SoumettreButton(),
+            SizedBox(height: 20),
+            // TODO: Ajoutez ici vos autres blocs
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
 class BilanContent extends StatelessWidget {
   final int eleveId;
 
@@ -15,16 +353,10 @@ class BilanContent extends StatelessWidget {
         cells: <DataCell>[
           const DataCell(Center(child: Text('1'))), // centrage du contenu
           const DataCell(Center(child: Text('12/07/2023'))),
-          const DataCell(Center(
-            child: Image(
-              image: AssetImage('assets/smiley/1.png'),
-              height: 20,
-              alignment: Alignment.center,
-            ),
-          )),
-          const DataCell(Center(child: Text('8'))),
-          const DataCell(Center(child: Text('9'))),
-          const DataCell(Center(child: Text('10'))),
+          const DataCell(Center(child: smiley1)),
+          const DataCell(Center(child: smiley2)),
+          const DataCell(Center(child: smiley3)),
+          const DataCell(Center(child: smiley5)),
           DataCell(
             Center(
               child: TextButton(
@@ -41,10 +373,10 @@ class BilanContent extends StatelessWidget {
         cells: <DataCell>[
           const DataCell(Center(child: Text('2'))),
           const DataCell(Center(child: Text('13/07/2023'))),
-          const DataCell(Center(child: Text('8'))),
-          const DataCell(Center(child: Text('9'))),
-          const DataCell(Center(child: Text('8'))),
-          const DataCell(Center(child: Text('7'))),
+          const DataCell(Center(child: smiley4)),
+          const DataCell(Center(child: smiley4)),
+          const DataCell(Center(child: smiley4)),
+          const DataCell(Center(child: smiley1)),
           DataCell(
             Center(
               child: TextButton(
@@ -64,10 +396,10 @@ class BilanContent extends StatelessWidget {
         cells: <DataCell>[
           const DataCell(Center(child: Text('1'))), // centrage du contenu
           const DataCell(Center(child: Text('12/07/2023'))),
-          const DataCell(Center(child: Text('7'))),
-          const DataCell(Center(child: Text('8'))),
-          const DataCell(Center(child: Text('9'))),
-          const DataCell(Center(child: Text('10'))),
+          const DataCell(Center(child: smiley5)),
+          const DataCell(Center(child: smiley4)),
+          const DataCell(Center(child: smiley4)),
+          const DataCell(Center(child: smiley2)),
           DataCell(
             Center(
               child: TextButton(
@@ -83,6 +415,11 @@ class BilanContent extends StatelessWidget {
     ],
     // Ajoutez plus d'élèves si nécessaire
   };
+
+
+
+
+
 
 
   BilanContent({required this.eleveId, Key? key}) : super(key: key);
@@ -168,27 +505,31 @@ class BilanContent extends StatelessWidget {
 
               const SizedBox(height: 20),
 
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Transform.scale(
+                  scale: 1.4,
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.blue,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => NewBilan()),
+                      );
+                    },
+                    tooltip: "Ajout d'un bilan",
+                    elevation: 6.0,
+                    shape: const CircleBorder(),
+                    child: add_bilan,
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
             ],
           ),
         ),
-      ),
-    );
-  }
-}
-
-
-// Modifiez BilanScreen pour qu'il utilise BilanContent dans son body
-class BilanScreen extends StatelessWidget {
-  final int eleveId;
-
-  const BilanScreen({required this.eleveId, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(color: orangePerso, context: context),
-      body: SingleChildScrollView(
-        child: BilanContent(eleveId: eleveId),
       ),
     );
   }
--- lib/utilities/constantes.dart
+++ lib/utilities/constantes.dart
@@ -3,3 +3,40 @@ import 'package:flutter/material.dart';
 const orangePerso = Colors.orangeAccent;
 const couleurIcone = Colors.white;
 
+const Image smiley1 = Image(
+  image: AssetImage('assets/smiley/1.png'),
+  height: 20,
+  alignment: Alignment.center,
+);
+
+const Image smiley2 = Image(
+  image: AssetImage('assets/smiley/2.png'),
+  height: 20,
+  alignment: Alignment.center,
+);
+
+const Image smiley3 = Image(
+  image: AssetImage('assets/smiley/3.png'),
+  height: 20,
+  alignment: Alignment.center,
+);
+
+const Image smiley4 = Image(
+  image: AssetImage('assets/smiley/4.png'),
+  height: 20,
+  alignment: Alignment.center,
+);
+
+const Image smiley5 = Image(
+  image: AssetImage('assets/smiley/5.png'),
+  height: 20,
+  alignment: Alignment.center,
+);
+
+
+const Image add_bilan = Image(
+  image: AssetImage('assets/divers/add_bilan.png'),
+  alignment: Alignment.center,
+);
+
+
