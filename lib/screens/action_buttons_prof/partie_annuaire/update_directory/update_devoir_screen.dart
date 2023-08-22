1d804fb154a7fe5ed3e141c653b0b44dadee432c
Colin COËRCHON <99185713+ColinAlAskA@users.noreply.github.com>
2023-08-22 17:58:03 +0200

    Alaska - commit du 22/08
    - RAS

---
+++ lib/screens/action_buttons_prof/partie_annuaire/update_directory/update_devoir_screen.dart
@@ -0,0 +1,5 @@
+// Fichier vide car finalement, on a simplement ajouté la fonctionnalité de pouvoir modifier l'état d'un devoir.
+
+// Les 3 états : FAIT / MOYENNEMENT FAIT / PAS FAIT
+
+// Peut-être d'autres modifications pour des versions futures :)
